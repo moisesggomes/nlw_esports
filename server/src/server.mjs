d1c753349debcaf43868712139988cf96f2ed228
Moisés Gomes <moisesgomes376@gmail.com>
2022-09-12 11:09:58 -0300

    Edit ad name

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -4,18 +4,9 @@ const app = express()
 
 app.get("/ads", (request, response) => {
   return response.json([
-    {
-      id: 1,
-      ads: "Anuncio 1"
-    },
-    {
-      id: 2,
-      ads: "Anuncio 2"
-    },
-    {
-      id: 3,
-      ads: "Anuncio 3"
-    },
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
   ])
 })
 
