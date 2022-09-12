b0ea894ff249b967e0c9b42fba1d729412e066d2
Moisés Gomes <moisesgomes376@gmail.com>
2022-09-12 13:27:56 -0300

    Play with React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text>
+        { props.title }
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Hello React Native!
+      </Text>
+
+      <Button title="Touch 1" />
+      <Button title="Touch 2" />
+      <Button title="Touch 3" />
+
+      <StatusBar style="auto" backgroundColor="lightgray" />
     </View>
   );
 }
@@ -13,8 +35,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    gap: 20,
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    fontSize: 20,
+    padding: 20,
+    borderRadius: 5,
+    backgroundColor: "salmon"
+  }
 });
