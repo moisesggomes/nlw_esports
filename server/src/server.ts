bba631f1b5614324bc02b4e4524af68f25060fd1
Moisés Gomes <moisesgomes376@gmail.com>
2022-09-12 11:44:53 -0300

    Setup for server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ app.get("/ads", (request, response) => {
     { id: 1, name: "Anuncio 1" },
     { id: 2, name: "Anuncio 2" },
     { id: 3, name: "Anuncio 3" },
+    { id: 4, name: "Anuncio 4" },
+    { id: 5, name: "Anuncio 5" },
   ])
 })
 
