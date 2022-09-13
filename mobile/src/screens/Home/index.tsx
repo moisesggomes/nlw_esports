7184a49e59cfc3d33eaa72ae7da7fad106efa60a
Moisés Gomes <moisesgomes376@gmail.com>
2022-09-13 14:38:15 -0300

    Create Heading component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -5,12 +5,19 @@ import logoImg from "../../assets/logo-nlw-esports.png"
 
 import { styles } from "./styles"
 
+import { Heading } from "../../components/Heading"
+
 export function Home() {
   return (
     <View style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
+      />
+
+      <Heading
+        title="Encontre seu duo"
+        subtitle="Selecione o game que deseja jogar..."
       />
     </View>
   )
