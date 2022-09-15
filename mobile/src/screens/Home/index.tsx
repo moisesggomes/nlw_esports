66eb01e695ca271546232ce4d5e295e8612bec57
Moises Gomes <moisesgomes376@gmail.com>
2022-09-15 19:22:51 -0300

    Install navigation dependencies

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
-import { View, Image, FlatList } from "react-native"
+import { Image, FlatList } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 import logoImg from "../../assets/logo-nlw-esports.png"
 
@@ -18,7 +19,7 @@ export function Home() {
   }, [])
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
@@ -37,6 +38,6 @@ export function Home() {
         horizontal
         contentContainerStyle={styles.contentList}
       />
-    </View>
+    </SafeAreaView>
   )
 }
