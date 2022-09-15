c46c8759210f0a8763badc59194e4fc25b9e5bb2
Moises Gomes <moisesgomes376@gmail.com>
2022-09-15 18:14:13 -0300

    Use fetch

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import { View, Image, FlatList } from "react-native"
 
 import logoImg from "../../assets/logo-nlw-esports.png"
@@ -9,7 +10,26 @@ import { GameCard, GameCardProps } from "../../components/GameCard"
 
 import { GAMES } from "../../utils/games"
 
+interface Game {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
+}
+
 export function Home() {
+  const [games, setGames] = useState<Game[]>([])
+
+
+
+  useEffect(() => {
+    fetch("http://10.0.0.106:3333")
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image
