53ee60b71f56571f573c56e6e86754fe0b03fb35
Moises Gomes <moisesgomes376@gmail.com>
2022-09-15 21:29:16 -0300

    Style DuoCard

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -1,10 +1,60 @@
-import { View } from "react-native"
+import { TouchableOpacity, View } from "react-native"
+import { GameController } from "phosphor-react-native"
+
+import { DuoInfo } from "../DuoInfo"
 
 import { styles } from "./styles"
+import { THEME } from "../../theme"
+import { Text } from "react-native"
 
-export function DuoCard() {
+export interface DuoCardProps {
+    hourEnd: string
+    hourStart: string
+    id: string
+    name: string
+    useVoiceChannel: boolean
+    weekDays: string[]
+    yearsPlaying: number
+}
+
+interface Props {
+    data: DuoCardProps
+}
+
+export function DuoCard({ data }: Props) {
     return (
         <View style={styles.container}>
+            <DuoInfo
+                label="Nome"
+                value={data.name}
+            />
+            <DuoInfo
+                label="Tempo de jogo"
+                value={`${data.yearsPlaying} anos`}
+            />
+            <DuoInfo
+                label="Disponibilidade"
+                value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+            />
+            <DuoInfo
+                label="Chamada de Áudio?"
+                value={data.useVoiceChannel ? "Sim" : "Não"}
+                colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+            >
+                <GameController
+                    color={THEME.COLORS.TEXT}
+                    size={20}
+                />
+
+                <Text style={styles.buttonTitle}>
+                    Conectar
+                </Text>
+
+            </TouchableOpacity>
 
         </View>
     )
